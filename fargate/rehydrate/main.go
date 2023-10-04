9fc23132e3fc1215820a9c04f671f25cf646e331
Edmore Moyo <etmoyo@gmail.com>
2023-10-03 22:35:38 -0400

    refactors

--- fargate/rehydrate/main.go
+++ fargate/rehydrate/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
-	"sync"
 
 	"github.com/pennsieve/pennsieve-go/pkg/pennsieve"
+	"github.com/pennsieve/pennsieve-go/pkg/pennsieve/models/discover"
 )
-
-var rehydrateProcessWg sync.WaitGroup
 
 func main() {
 	log.Println("Running rehydrate task")
@@ -22,11 +21,11 @@ func main() {
 
 	datasetId, err := strconv.Atoi(os.Getenv("DATASET_ID"))
 	if err != nil {
-		log.Fatalf("err converting datasetId to int")
+		log.Fatalf("error converting datasetId to int")
 	}
 	versionId, err := strconv.Atoi(os.Getenv("DATASET_VERSION_ID"))
 	if err != nil {
-		log.Fatalf("err converting versionId to int")
+		log.Fatalf("error converting versionId to int")
 	}
 
 	pennsieveClient := pennsieve.NewClient(pennsieve.APIParams{ApiHost: getApiHost(os.Getenv("ENV"))})
@@ -42,18 +41,29 @@ func main() {
 	}
 	log.Println(datasetMetadataByVersionReponse)
 
-	log.Println("Starting Rehaydration process")
-	for i := 1; i <= 20; i++ {
-		rehydrateProcessWg.Add(1)
+	numberOfJobs := len(datasetMetadataByVersionReponse.Files)
+	jobs := make(chan discover.DatasetFile, numberOfJobs)
+	results := make(chan string, numberOfJobs)
 
-		go func(i int) {
-			defer rehydrateProcessWg.Done()
-			worker(i)
-		}(i)
+	log.Println("Starting Rehydration process")
+	// create workers
+	NumConcurrentWorkers := 20
+	for i := 1; i <= NumConcurrentWorkers; i++ {
+		go worker(i, jobs, results)
 	}
 
-	rehydrateProcessWg.Wait()
-	log.Println("Rehaydration complete")
+	// create work
+	for _, j := range datasetMetadataByVersionReponse.Files {
+		jobs <- j
+	}
+	close(jobs)
+
+	// wait for the done signal
+	for j := 1; j <= numberOfJobs; j++ {
+		log.Println(<-results)
+	}
+
+	log.Println("Rehydration complete")
 }
 
 func getApiHost(env string) string {
@@ -64,8 +74,12 @@ func getApiHost(env string) string {
 	}
 }
 
-func worker(w int) {
-	log.Println("starting ", w)
-	log.Printf("%v done", w)
+func worker(w int, jobs <-chan discover.DatasetFile, results chan<- string) {
+	for j := range jobs {
+		log.Println("starting ", w)
+		log.Println("processing ", j)
+
+		results <- fmt.Sprintf("%v done", w)
+	}
 
 }

f5991f8a7a3c156c4909abebdbed46c865b3a806
Edmore Moyo <etmoyo@gmail.com>
2023-10-30 13:57:02 -0400

    adds TODOs for debug statements to be removed

--- fargate/rehydrate/main.go
+++ fargate/rehydrate/main.go
@@ -38,13 +38,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("error retrieving dataset by version")
 	}
-	log.Println(datasetByVersionReponse)
+	log.Println(datasetByVersionReponse) // TODO remove
 
 	datasetMetadataByVersionReponse, err := pennsieveClient.Discover.GetDatasetMetadataByVersion(ctx, int32(datasetId), int32(versionId))
 	if err != nil {
 		log.Fatalf("error retrieving dataset by version")
 	}
-	log.Println(datasetMetadataByVersionReponse)
+	log.Println(datasetMetadataByVersionReponse) // TODO remove
 
 	// Initializing environment
 	cfg, err := config.LoadDefaultConfig(context.Background())
@@ -76,7 +76,7 @@ func main() {
 		if err != nil {
 			log.Fatalf("error retrieving dataset file by version")
 		}
-		log.Println(datasetFileByVersionResponse)
+		log.Println(datasetFileByVersionResponse) // TODO remove
 
 		rehydrations <- NewRehydration(
 			SourceObject{

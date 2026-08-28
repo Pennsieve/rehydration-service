0443e9daebdff7b7dd47f59c922f21cdec88dedc
Michael Uftring <michael.uftring@gmail.com>
2026-08-28 14:03:44 -0400

    Grant s3:PutObjectTagging on rehydration bucket for fargate task role
    CopyObject defaults to TaggingDirective=COPY, so rehydration's simple-copy
    path attempts to write the source object's tags onto the destination
    object in the rehydration bucket. The fargate task role could read source
    tags (s3:Get* on the publish bucket) but lacked s3:PutObjectTagging on the
    destination, causing every file copy to fail with a 403 AccessDenied.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -72,6 +72,7 @@ data "aws_iam_policy_document" "rehydration_fargate_iam_policy_document" {
 
     actions = [
       "s3:PutObject",
+      "s3:PutObjectTagging",
       "s3:DeleteObject",
       "s3:ListBucket",
       "s3:AbortMultipartUpload"
